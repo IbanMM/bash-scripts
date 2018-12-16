cb9614d0475be74bacf797cbd7c5ce40b0af5166
iban moreno <iban.moreno.martin@gmail.com>
2018-12-16 12:59:47 +0100

    Script for install WP

---
+++ wp_install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# WodPress install require WP Cli
+
+# Colors for the outputs uses tput
+GREEN="$(tput setaf 2)"
+CLEAR="$(tput sgr0)"
+
+# User input path for the installation folder
+read -e -p 'Path to install WP (absolute) : ' -i '/srv/http/' wppath
+
+# Database creation
+echo 'Lest create a database for WordPress'
+read -p 'Name of the database : ' dbname
+read -p 'User of the database : ' dbuser
+read -p 'Password for the database : ' dbpassword
+read -p 'Password for mysql root user : ' mysqlpassword
+
+SQL1="CREATE DATABASE $dbname;"
+SQL2="CREATE USER '$dbuser'@'localhost' IDENTIFIED BY '$dbpassword';"
+SQL3="GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'localhost' IDENTIFIED BY '$dbpassword';"
+SQL4="FLUSH PRIVILEGES;"
+
+mysql -h localhost -u root -p$mysqlpassword -Bse "$SQL1$SQL2$SQL3$SQL4"
+
+echo $GREEN'Database created correctly'$CLEAR
+
+# cd in the target folder for the WP installation
+cd $wppath
+
+# Download WP
+read -e -p 'Locale for the WordPress installation : ' -i 'es_ES' wplocale
+wp core download --locale=$wplocale
+
+# Generate wp-config.php
+wp config create --dbname=$dbname --dbuser=$dbuser --dbpass=$dbpassword --locale=$wplocale
+
+# Install WP
+read -p 'URL for the WordPress installation : ' wpurl
+read -p 'Title for the WordPress installation : ' wptitle
+read -p 'User name for admin user : ' wpadmin
+read -p 'Email of the admin user : ' wpadminemail
+read -p 'Password for the admin user : ' wpadminpassword 
+wp core install --url=$wpurl --title=$wptitle --admin_user=$wpadmin --admin_password='$wpadminpassword' --admin_email=$wpadminemail
